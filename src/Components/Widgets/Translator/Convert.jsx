153a7f6c50445e9a47e02bcb2ae57404ad2b55a5
Maxks <maxksimronn@gmail.com>
2021-03-02 18:45:42 +0200

    Finished working on Translator App

---
+++ src/Components/Widgets/Translator/Convert.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import GoogleTranslation from "../../../api/GoogleTranslation";
+
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() => {
+    console.log("debounced useEffect");
+    const timer = setTimeout(() => setDebouncedText(text), 2000);
+
+    return () => clearTimeout(timer);
+  }, [text]);
+
+  useEffect(() => {
+    console.log("useEffect of lang and text");
+    const translate = async () => {
+      try {
+        const { data } = await GoogleTranslation.post("", null, {
+          params: {
+            q: debouncedText,
+            target: language.value,
+          },
+        });
+
+        setTranslated(data.data["translations"][0]["translatedText"]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    if (debouncedText) {
+      translate().then(null);
+    }
+  }, [debouncedText, language]);
+
+  return (
+    <div className="ui segment">
+      <h3 className="ui header" lang={language.value}>
+        {translated}
+      </h3>
+    </div>
+  );
+};
+
+export default Convert;
--- src/Components/Widgets/Translator/Translator.jsx
+++ src/Components/Widgets/Translator/Translator.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
   {
@@ -23,11 +24,31 @@ const options = [
     label: "Russian",
     value: "ru",
   },
+  {
+    label: "Italian",
+    value: "it",
+  },
+  {
+    label: "Ukrainian",
+    value: "uk",
+  },
+  {
+    label: "Portuguese",
+    value: "pt",
+  },
+  {
+    label: "Romanian",
+    value: "ro",
+  },
+  {
+    label: "Czech",
+    value: "cs",
+  },
 ];
 
 const Translator = () => {
   const [language, setLanguage] = useState(options[0]);
-  const [text, setText] = useState("Some text");
+  const [text, setText] = useState("");
 
   return (
     <div>
@@ -39,7 +60,7 @@ const Translator = () => {
           <input
             id="text"
             type="text"
-            placeholder={text}
+            placeholder="Enter some text to translate"
             onChange={(e) => setText(e.target.value)}
           />
         </div>
@@ -50,6 +71,7 @@ const Translator = () => {
         onSelectedLangChange={setLanguage}
         label="Select a Language"
       />
+      <Convert language={language} text={text} />
     </div>
   );
 };
