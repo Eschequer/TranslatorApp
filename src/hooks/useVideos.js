742ada43ae1682953a0a3cea5f7a497d2dfcae65
Maxks <maxksimronn@gmail.com>
2021-03-03 23:18:55 +0200

    Created useVideos custom hook in hooks folder

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import youtube from "../api/youtube";
+
+function useVideos(defaultSearchTerm) {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    searchVideos(defaultSearchTerm).catch((error) =>
+      console.dir(error.message)
+    );
+  }, []);
+
+  async function searchVideos(term) {
+    const { data } = await youtube.get("/search", {
+      params: { q: term },
+    });
+
+    setVideos(data.items);
+  }
+
+  return [videos, searchVideos];
+}
+
+export default useVideos;
